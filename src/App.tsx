18433c8b00844e666545c0deef5cd5cb07642686
Amanda Albuquerque <amandlei@hotmail.com>
2023-04-27 12:59:41 -0300

    feat(text-input): add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Button from './components/Button/Button';
+import TextInput from './components/TextInput/TextInput';
 
 const App: React.FC = () => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
       <h1>UI Library Demo</h1>
+
+      <TextInput
+        id="example-input"
+        placeholder="Enter your text here"
+        onChange={handleInputChange}
+      />
       <Button onClick={() => alert('Button clicked!')}>Click me!</Button>
     </div>
   );
